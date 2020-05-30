822b803f87247e563f44a1137decc9adeb089a16
Wbb <cgnbin@qq.com>
2020-05-30 23:36:01 +0800

    Update patch_kernel_5.4_self1.sh

--- patch_kernel_5.4_self1.sh
+++ patch_kernel_5.4_self1.sh
@@ -1,5 +1,7 @@
 cd friendlywrt-rk3328
 cd kernel/
+git checkout .
+git clean -fd
 git apply ../../add_fullconenat.diff
 wget https://github.com/armbian/build/raw/master/patch/kernel/rockchip64-dev/RK3328-enable-1512mhz-opp.patch
 git apply RK3328-enable-1512mhz-opp.patch
@@ -14,6 +16,7 @@ cp -a ./target/linux/generic/files/* ../kernel/
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/pending-5.4
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/hack-5.4
 cd ../
+rm merge_config.sh* -rf
 wget https://github.com/torvalds/linux/raw/master/scripts/kconfig/merge_config.sh && chmod +x merge_config.sh
 grep -i '_NETFILTER_\|FLOW' ../.config.override > .config.override
 ./merge_config.sh -m .config.override kernel/arch/arm64/configs/nanopi-r2_linux_defconfig && mv .config kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
